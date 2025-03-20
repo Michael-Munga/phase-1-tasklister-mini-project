1a77b2c944dd20e38f2ab117287176cf82fc52c5
Michael Munga <micknyalle@gmail.com>
2025-03-20 19:05:53 +0300

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form");
+  let taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let taskInput = document.getElementById("new-task-description");
+    let taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      let newTask = document.createElement("li");
+      newTask.textContent = taskText + " ";
+
+      let deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => newTask.remove());
+
+      newTask.appendChild(deleteButton);
+      taskList.appendChild(newTask);
+
+      taskInput.value = "";
+    }
+  });
 });
